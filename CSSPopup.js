82918e391dc65957830e44b82d6716f3cee7c3c1
shitao1988 <shitao19880316@gmail.com>
2012-09-14 17:07:39 +0800

    第一个版本
    第一个版本

---
+++ CSSPopup.js
@@ -0,0 +1,260 @@
+
+OpenLayers.CSSPopup = OpenLayers.Class(OpenLayers.Popup.Framed, {
+    autoSize: true,
+    panMapIfOutOfView: true,
+    fixedRelativePosition: false,
+
+    positionBlocks: {
+        "tl": {
+            'offset': new OpenLayers.Pixel(44, -6),
+            'padding': new OpenLayers.Bounds(5, 14, 5, 5),
+            'blocks': [
+                { 
+                    className: 'olwidgetPopupStemTL',
+                    size: new OpenLayers.Size(24, 14),
+                    anchor: new OpenLayers.Bounds(null, 0, 32, null),
+                    position: new OpenLayers.Pixel(0, -28)
+                }
+            ]
+        },
+        "tr": {
+            'offset': new OpenLayers.Pixel(-44, -6),
+            'padding': new OpenLayers.Bounds(5, 14, 5, 5),
+            'blocks': [
+                { 
+                    className: "olwidgetPopupStemTR",
+                    size: new OpenLayers.Size(24, 14),
+                    anchor: new OpenLayers.Bounds(32, 0, null, null),
+                    position: new OpenLayers.Pixel(0, -28)
+                }
+            ]
+        },
+        "bl": {
+            'offset': new OpenLayers.Pixel(44, 6),
+            'padding': new OpenLayers.Bounds(5, 5, 5, 14),
+            'blocks': [
+                { 
+                    className: "olwidgetPopupStemBL",
+                    size: new OpenLayers.Size(24, 14),
+                    anchor: new OpenLayers.Bounds(null, null, 32, 0),
+                    position: new OpenLayers.Pixel(0, 0)
+                }
+            ]
+        },
+        "br": {
+            'offset': new OpenLayers.Pixel(-44, 6),
+            'padding': new OpenLayers.Bounds(5, 5, 5, 14),
+            'blocks': [
+                { 
+                    className: "olwidgetPopupStemBR",
+                    size: new OpenLayers.Size(24, 14),
+                    anchor: new OpenLayers.Bounds(32, null, null, 0),
+                    position: new OpenLayers.Pixel(0, 0)
+                }
+            ]
+        }
+    },
+
+    initialize: function(id, lonlat, contentSize, contentHTML, anchor, closeBox,
+                    closeBoxCallback, relativePosition, separator) {
+        if (relativePosition && relativePosition != 'auto') {
+            this.fixedRelativePosition = true;
+            this.relativePosition = relativePosition;
+        }
+        if (separator === undefined) {
+            this.separator = ' of ';
+        } else {
+            this.separator = separator;
+        }
+
+        this.olwidgetCloseBox = closeBox;
+        this.olwidgetCloseBoxCallback = closeBoxCallback;
+        this.page = 0;
+        OpenLayers.Popup.Framed.prototype.initialize.apply(this, [id, lonlat,
+            contentSize, contentHTML, anchor, false, null]);
+    },
+
+    /*
+     * 构造popup内部容器。如果内部是一个数组则分页显示
+     */
+    setContentHTML: function(contentHTML) {
+        if (contentHTML !== null && contentHTML !== undefined) {
+            this.contentHTML = contentHTML;
+        }
+
+        var pageHTML;
+        var showPagination;
+        if (this.contentHTML.constructor != Array) {
+            pageHTML = this.contentHTML;
+            showPagination = false;
+        } else {
+            pageHTML = this.contentHTML[this.page];
+            showPagination = this.contentHTML.length > 1;
+        }
+
+        if ((this.contentDiv !== null) && (pageHTML !== null)) {
+            var popup = this; 
+
+            // 清空旧数据
+            this.contentDiv.innerHTML = "";
+
+            // 创建内部容器
+            var containerDiv = document.createElement("div");
+            containerDiv.className = 'olwidgetPopupContent';
+            this.contentDiv.appendChild(containerDiv);
+
+            // 创建关闭按钮
+            if (this.olwidgetCloseBox) {
+                var closeDiv = document.createElement("div");
+                closeDiv.className = "olwidgetPopupCloseBox";
+                closeDiv.innerHTML = "close";
+                closeDiv.onclick = function(event) {
+                    popup.olwidgetCloseBoxCallback.apply(popup, arguments);
+                };
+                this.contentDiv.appendChild(closeDiv);
+            }
+
+            var pageDiv = document.createElement("div");
+            pageDiv.innerHTML = pageHTML;
+            pageDiv.className = "olwidgetPopupPage";
+            containerDiv.appendChild(pageDiv);
+
+            if (showPagination) {
+                // 创建分页控件
+                var paginationDiv = document.createElement("div");
+                paginationDiv.className = "olwidgetPopupPagination";
+                var prev = document.createElement("div");
+                prev.className = "olwidgetPaginationPrevious";
+                prev.innerHTML = "prev";
+                prev.onclick = function(event) {
+                    popup.page = (popup.page - 1 + popup.contentHTML.length) %
+                        popup.contentHTML.length;
+                    popup.setContentHTML();
+                    popup.map.events.triggerEvent("move");
+                };
+
+                var count = document.createElement("div");
+                count.className = "olwidgetPaginationCount";
+                count.innerHTML = (this.page + 1) + this.separator + this.contentHTML.length;
+                var next = document.createElement("div");
+                next.className = "olwidgetPaginationNext";
+                next.innerHTML = "next";
+                next.onclick = function(event) {
+                    popup.page = (popup.page + 1) % popup.contentHTML.length;
+                    popup.setContentHTML();
+                    popup.map.events.triggerEvent("move");
+                };
+
+                paginationDiv.appendChild(prev);
+                paginationDiv.appendChild(count);
+                paginationDiv.appendChild(next);
+                containerDiv.appendChild(paginationDiv);
+
+            }
+            var clearFloat = document.createElement("div");
+            clearFloat.style.clear = "both";
+            containerDiv.appendChild(clearFloat);
+
+            if (this.autoSize) {
+                this.registerImageListeners();
+                this.updateSize();
+            }
+        }
+    },
+
+    /*
+     * 重写createBlocks：使用CSS样式而不是特定的img图片
+     */
+    createBlocks: function() {
+        this.blocks = [];
+
+        // since all positions contain the same number of blocks, we can
+        // just pick the first position and use its blocks array to create
+        // our blocks array
+        var firstPosition = null;
+        for(var key in this.positionBlocks) {
+            firstPosition = key;
+            break;
+        }
+
+        var position = this.positionBlocks[firstPosition];
+        for (var i = 0; i < position.blocks.length; i++) {
+
+            var block = {};
+            this.blocks.push(block);
+
+            var divId = this.id + '_FrameDecorationDiv_' + i;
+            block.div = OpenLayers.Util.createDiv(divId,
+                null, null, null, "absolute", null, "hidden", null
+            );
+            this.groupDiv.appendChild(block.div);
+        }
+    },
+    /*
+     * 重写updateBlocks
+     */
+    updateBlocks: function() {
+        if (!this.blocks) {
+            this.createBlocks();
+        }
+        if (this.size && this.relativePosition) {
+            var position = this.positionBlocks[this.relativePosition];
+            for (var i = 0; i < position.blocks.length; i++) {
+
+                var positionBlock = position.blocks[i];
+                var block = this.blocks[i];
+
+                // adjust sizes
+                var l = positionBlock.anchor.left;
+                var b = positionBlock.anchor.bottom;
+                var r = positionBlock.anchor.right;
+                var t = positionBlock.anchor.top;
+
+                // note that we use the isNaN() test here because if the
+                // size object is initialized with a "auto" parameter, the
+                // size constructor calls parseFloat() on the string,
+                // which will turn it into NaN
+                //
+                var w = (isNaN(positionBlock.size.w)) ? this.size.w - (r + l)
+                                                      : positionBlock.size.w;
+
+                var h = (isNaN(positionBlock.size.h)) ? this.size.h - (b + t)
+                                                      : positionBlock.size.h;
+
+                block.div.style.width = (w < 0 ? 0 : w) + 'px';
+                block.div.style.height = (h < 0 ? 0 : h) + 'px';
+
+                block.div.style.left = (l !== null) ? l + 'px' : '';
+                block.div.style.bottom = (b !== null) ? b + 'px' : '';
+                block.div.style.right = (r !== null) ? r + 'px' : '';
+                block.div.style.top = (t !== null) ? t + 'px' : '';
+
+                block.div.className = positionBlock.className;
+            }
+
+            this.contentDiv.style.left = this.padding.left + "px";
+            this.contentDiv.style.top = this.padding.top + "px";
+        }
+    },
+    updateSize: function() {
+    	//popup是否在地图视图内
+        if (false) {
+            var preparedHTML = "<div class='" + this.contentDisplayClass+ "'>" +
+                this.contentDiv.innerHTML +
+                "</div>";
+
+            var containerElement = document.body;
+            var realSize = OpenLayers.Util.getRenderedDimensions(
+                preparedHTML, null, {
+                    displayClass: this.displayClass,
+                    containerElement: containerElement
+                }
+            );
+            return this.setSize(realSize);
+        } else {
+            return OpenLayers.Popup.prototype.updateSize.apply(this, arguments);
+        }
+    },
+
+    CLASS_NAME: "OpenLayers.CSSPopup"
+});
